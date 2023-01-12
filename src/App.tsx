06c8bad3034d997d666ea8b63c21c98898fb72e0
Đặng Quang Nguyên <quangnguyenne1411@gmail.com>
2023-01-12 14:36:42 +0700

    [Router] update router source

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,49 @@
 import React from "react";
 
-import Button from "../src/Atomic/atoms/Button";
+import { Switch, Route, BrowserRouter } from 'react-router-dom';
+
+import _map from "lodash/map";
+
+interface RouteProps {
+  path: string;
+  component: React.ReactNode;
+}
 
 export default function App() {
+  const pageRoutes = [
+    {
+      path: "/login",
+      component: <>Login</>,
+    },
+    {
+      path: "/register",
+      component: <>register</>,
+    },
+    {
+      path: "/dashboard/user",
+      component: <>dashboard/user</>,
+    },
+    {
+      path: "/dashboard/articles",
+      component: <>/dashboard/articles</>,
+    },
+    {
+      path: "/404",
+      component: <>Page not found</>,
+    },
+  ];
+
   return (
     <>
-      <Button>123</Button>
+      <BrowserRouter>
+        <Switch>
+          {_map(pageRoutes, (route: RouteProps, idx: any) => (
+            <Route key={idx.toString()} path={route.path}>
+              {route.component}
+            </Route>
+          ))}
+        </Switch>
+      </BrowserRouter>
     </>
   );
 }
